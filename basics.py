7f153ea655967dea702b88d1b27c28e6419d8aa1
Aaron McNeal <aaron.mcneal@ibm.com>
2024-02-12 13:27:22 -0600

    initial commit

---
+++ basics.py
@@ -0,0 +1,25 @@
+firstName = "aaron"
+lastName = "mcneal"
+age = 30
+print(type(firstName))
+print(type(age))
+print(firstName.upper())
+
+properFirstName = firstName.capitalize()
+properLastName = lastName.capitalize()
+
+varToSplit = "Aaron, McNeal"
+print(varToSplit.split(','))
+
+year = "2024"
+print(type(year))
+print(type(int(year)))
+
+x = 7
+# x = x + 1 Same as the code below
+x += 1
+x *= 2
+print(x == 16)
+
+myList = ["Black", "Blue", "Grey"]
+newList = list((firstName, lastName, age))
