492e30088e82b941415ecc078a7f1664913ec519
Aaron McNeal <aaron.mcneal@ibm.com>
2024-02-13 10:56:43 -0600

    Continued developing Python skills

---
+++ modules.py
@@ -0,0 +1,7 @@
+import math
+
+# See what functions the module contains by using the dir function
+print(dir(math))
+# Use the help function for a description on the specific function you'll like to use
+print(help(math.sqrt))
+math.sqrt(74)
